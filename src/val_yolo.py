a5e6a904bf403803eb77e403f026e8cca037d3c4
Aswin Babu <63053141+aswin-roman@users.noreply.github.com>
2022-10-14 12:00:14 +0200

    Removed unwanted runs

--- src/val_yolo.py
+++ src/val_yolo.py
@@ -2,10 +2,11 @@ from yolov5 import val
 from PIL import Image
 import mlflow
 import yaml
-import json
 import os
 
 def main():
+    mlflow.set_experiment("License plate detection using YOLOv5")
+    
     #start mlfow
     mlflow.start_run()
     
@@ -15,7 +16,7 @@ def main():
 
     #do validation
     #metrics = (mp, mr, map50, map, speed)
-    metrics, maps, t = val.run(weights='runs/train/run4/weights/best.pt',
+    metrics, maps, t = val.run(weights='models/best_latest.pt',
                   project='runs/val',
                   name='run',
                   exist_ok=True,
@@ -26,13 +27,13 @@ def main():
                   iou_thres=params_val["iou_thres"])
     
     #log parameters
-    mlflow.log_param("Dataset", f"{os.path.abspath(os.curdir)}/data/ai-dataset-dvc-poc/data/OpenALPR/")
-    mlflow.log_param("Epochs", params_train["epochs"])
-    mlflow.log_param("Batch size", params_train["batch_size"])
+    mlflow.log_param("Dataset", "/data/ai-dataset-dvc-poc/data/OpenALPR/")
+    mlflow.log_param("Training epochs", params_train["epochs"])
+    mlflow.log_param("Training Batch size", params_train["batch_size"])
     mlflow.log_param("Confidence threshold", params_val["conf_thres"])
     mlflow.log_param("IoU threshold", params_val["iou_thres"])
     
-    #save the validation results to json file
+    #validation results
     val_results = {
         "mAP_IoU_.5-.95": metrics[3],
         "mAP_IoU_0.50": metrics[2],
@@ -42,15 +43,19 @@ def main():
     #log metrics
     mlflow.log_metrics(val_results)
     
-    #log plots as images
+    #log plots 
+    if(os.path.exists("runs/train/run/results.png")):
+        train_results = Image.open("runs/train/run/results.png")
+        train_results = train_results.resize((1280, 720))
+        mlflow.log_image(train_results,"models/train_results.png")
     if(os.path.exists("runs/val/run/F1_curve.png")):
         F1 = Image.open("runs/val/run/F1_curve.png")
-        F1 = F1.resize((640, 430))
-        mlflow.log_image(F1, "models/F1_curve_val.png")
+        F1 = F1.resize((740, 590))
+        mlflow.log_image(F1, "models/F1.png")
     if(os.path.exists("runs/val/run/PR_curve.png")):
         PR = Image.open("runs/val/run/PR_curve.png")
-        PR = PR.resize((640, 430))
-        mlflow.log_image(PR, "models/PR_curve_val.png")
+        PR = PR.resize((740, 590))
+        mlflow.log_image(PR, "models/PR.png")
         
     #stop mlfow
     mlflow.end_run()
